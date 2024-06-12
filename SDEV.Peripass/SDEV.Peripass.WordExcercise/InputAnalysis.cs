ebc43628eced7d02625e549257e97f55eb4e145c
Sofie Delaeter <sofie.delaeter@delaware.pro>
2024-06-12 16:16:37 +0200

    project setup

---
+++ SDEV.Peripass/SDEV.Peripass.WordExcercise/InputAnalysis.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using SDEV.Peripass.WordExcercise.Models;
+
+namespace SDEV.Peripass.WordExcercise
+{
+    class InputAnalysis
+    {
+        private readonly ILogger<InputAnalysis> _logger;
+
+        public InputAnalysis(ILogger<InputAnalysis> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task Start(ScriptOptions options)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ SDEV.Peripass/SDEV.Peripass.WordExcercise/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CommandLine;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SDEV.Peripass.WordExcercise;
+using SDEV.Peripass.WordExcercise.Models;
+
+
+
+var scriptOptions = Parser.Default.ParseArguments<ScriptOptions>(args).MapResult(
+    (opts) => ScriptOptions.RunOptionsAndReturnExitCode(opts),
+    (errs) => ScriptOptions.HandleParseError(errs)
+);
+if (scriptOptions == null) { return; };
+
+var serviceCollection = ConfigureServices(scriptOptions);
+
+using (var serviceProvider = serviceCollection.BuildServiceProvider())
+{
+    var logger = serviceProvider.GetService<ILogger<Program>>();
+    if (logger is not null && serviceCollection is not null)
+    {
+        try
+        {
+            InputAnalysis inputAnalyser = serviceProvider.GetService<InputAnalysis>();
+            inputAnalyser.Start(scriptOptions).Wait();
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e.Message);
+            logger.LogError(e.StackTrace);
+        }
+        logger.LogInformation("Operation completed - {0}", DateTime.Now);
+    }
+    else
+    {
+        Console.WriteLine("Empty logger, could not start program");
+    }
+}
+
+static ServiceCollection ConfigureServices(ScriptOptions options)
+{
+    var serviceCollection = new ServiceCollection();
+
+    serviceCollection.AddTransient<InputAnalysis>();
+
+    serviceCollection.AddLogging((builder) =>
+    {
+        builder.AddConsole();
+    });
+
+    return serviceCollection;
+}
